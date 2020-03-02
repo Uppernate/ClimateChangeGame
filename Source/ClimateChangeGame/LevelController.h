b3be069bab0a1dc9debd2aafce38aa820dd564eb
Uppernate <uppernatearts@gmail.com>
2020-03-02 12:53:59 +0000

    Some Generic Classes, Camera Controls

---
+++ Source/ClimateChangeGame/LevelController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "LevelController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLIMATECHANGEGAME_API ALevelController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
